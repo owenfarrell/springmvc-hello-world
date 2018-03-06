972328050629bf2c339871a15e1dd377ee3c8f94
Owen Farrell <owen.farrell@gmail.com>
2018-03-06 15:27:03 -0500

    Upgraded to Spring Boot 2.0.0.RELEASE

--- src/main/java/com/example/config/HelloWorldConfiguration.java
+++ src/main/java/com/example/config/HelloWorldConfiguration.java
@@ -7,20 +7,19 @@ import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @ComponentScan(basePackages = "com.example")
 @Configuration
 @EnableWebMvc
-public class HelloWorldConfiguration extends WebMvcConfigurerAdapter {
+public class HelloWorldConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("redirect:/hello");
         registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
-        super.addViewControllers(registry);
     }
 
     @Override
